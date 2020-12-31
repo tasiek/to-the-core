14505a9f686f355e034ce7d53cff394308406de4
Łukasz Siewiorek <lukasz.siewiorek@vml.com>
2020-12-31 17:51:46 +0100

    intro text pos

--- src/scenes/Intro.ts
+++ src/scenes/Intro.ts
@@ -15,7 +15,7 @@ export default class Intro extends Base {
 
   createIntroText() {
     this.add.text(
-      this.getX(0.5), this.getY(0.5), 
+      this.getX(0.5), this.getY(0.4), 
       'To the core',  
       { 
         fontFamily: 'Megrim, Century Gothic, CenturyGothic, AppleGothic, sans-serif', 
