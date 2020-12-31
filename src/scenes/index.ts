df12e382317e7832db5ca252c9a02f87e540b632
Łukasz Siewiorek <lukasz.siewiorek@vml.com>
2020-12-31 15:03:51 +0100

    dist/ re-added

--- src/scenes/index.ts
+++
@@ -1,6 +0,0 @@
-export { Boot } from './Boot';
-export { Preloader } from './Preloader';
-export { Base } from './Base';
-export { Game } from './Game';
-export { GameTimer } from './GameTimer';
-export { Intro } from './Intro';
